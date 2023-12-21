35c571a95a63cfd4dd82e41ba45012be73ae1bdc
Arj09 <arjunkushwah100199@gmail.com>
2023-12-22 01:34:08 +0530

    add

--- src/Component/Add.js
+++ src/Component/Add.js
@@ -1,26 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom'
 import "../App.css"
+import axios from "axios";
 
 export const  Add = ()=>{
-
+    const [data, setData] = useState({})
     const navigate = useNavigate()
+    const [number, setNumber] = useState(0)
 
     const handleBack = ()=>{
         navigate("/")
+        
     }
+
+
+    const handleData = (e)=>{
+        const name = e.target.name
+        const value = e.target.value
+        setData(data=>({...data, [name]: value}))
+
+    }
+
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        setNumber(number=>number+1)
+       
+        console.log(data)
+        console.log(number)
+    }
+
+
+    useEffect(()=>{
+       
+        axios.post('http://localhost:5000/api/data', {
+
+            Company_Name : data.Company_Name,
+            City: data.City,
+            Starting_Year: data.Starting_Year,
+            Founders: data.Founders,
+            Industry: data.Industry,
+            No_of_Employees: data.No_of_Employees,
+            Funding_Amount: data.Funding_Amount
+        
+        }).then((res)=>{
+            console.log(res.data)
+            navigate("/")
+            
+        }).catch((err)=>{
+            console.log(err)
+           
+        })
+    }, [number])
     return(
         <>
         <button class='flex flex-row mx-5 my-5 px-6 py-1 bg-red-600 text-white rounded cursor-pointer' onClick={handleBack}>Back</button>
-        <form class="flex flex-col mx-auto  border-2 border-slate-100 w-4/5 px-5 pt-5 pb-10 gap-3 rounded my-24 lg:w-2/5 md:w-3/5  "  >
+
+
+        <form onSubmit={handleSubmit} class="flex flex-col mx-auto  border-2 border-slate-100 w-4/5 px-5 pt-5 pb-10 gap-3 rounded my-24 lg:w-2/5 md:w-3/5  "  >
             <text class="flex flex-row justify-center text-slate-900 pb-5 text-2xl">Add Startup data</text>
-            <input placeholder="Enter Company Name"  class="border-gray-100 border-2 pl-1" />
-            <input placeholder="Enter City"  class="border-gray-100 border-2 pl-1" />
-            <input placeholder="Enter Starting Year"  class="border-gray-100 border-2 pl-1" />
-            <input placeholder="Enter Founders"  class="border-gray-100 border-2 pl-1" />
-            <input placeholder="Enter Industry"  class="border-gray-100 border-2 pl-1" />
-            <input placeholder="Enter No of Employees"  class="border-gray-100 border-2 pl-1" />
-            <input placeholder="Enter Funding_Amount"  class="border-gray-100 border-2 pl-1" />
+            <input placeholder="Enter Company Name"  class="border-gray-100 border-2 pl-1"  name="Company_Name" value={data.Company_Name}  onChange={handleData}/>
+            <input placeholder="Enter City"  class="border-gray-100 border-2 pl-1"  name="City" value={data.City} onChange={handleData} />
+            <input placeholder="Enter Starting Year"  class="border-gray-100 border-2 pl-1"  name="Starting_Year" value={data.Starting_Year} onChange={handleData}/>
+            <input placeholder="Enter Founders"  class="border-gray-100 border-2 pl-1" name="Founders" value={data.Founders} onChange={handleData} />
+            <input placeholder="Enter Industry"  class="border-gray-100 border-2 pl-1" name="Industry"  value={data.Industry}  onChange={handleData}/>
+            <input placeholder="Enter No of Employees"  class="border-gray-100 border-2 pl-1"  name="No_of_Employees" value={data.No_of_Employees} onChange={handleData} />
+            <input placeholder="Enter Funding Amount"  class="border-gray-100 border-2 pl-1" name="Funding_Amount" value={data.Funding_Amount} onChange={handleData} />
             <button class="bg-red-600 text-white px-5 py-1 rounded" >Add data</button>
 
 
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -10,6 +10,7 @@ function Home() {
   const [data, setData] = useState([])
   const [key, setKey] = useState()
   const [industry, setIndustry] = useState('All')
+  const [Search, setSearch] = useState("")
   
   const navigate = useNavigate()
 
@@ -31,6 +32,10 @@ function Home() {
   const Industry = () => {
     return [...new Set(data.map((data) => data.Industry))];
   };
+
+  const handleIndustry =(e)=>{
+    setIndustry(e.target.value)
+  }
   
 
 
@@ -50,26 +55,35 @@ function Home() {
     <div class="w-4/5  bg-red-600  mx-auto my-2.5 rounded flex flex-row justify-between px-5 h-15 relative   text-center">
         <div  class="py-2">
             <text class='text-white pr-3'>Filter</text>
-            <select>
-                <option>Industry</option>
-                <option>HealthCare</option>
-                <option>Electronoce</option>
-                <option>technologyy</option>
-                <option>Al</option>
+            
+            <select  value={industry} onChange={handleIndustry} >
+                <option value="All">All</option>
+                {Industry().map((gender1) => {
+                  return (
+                      <option value={gender1} key={gender1}>
+                        {gender1}
+                      </option>
+                      );
+                })}
+                
             </select>
         </div>
+        <input placeholder=' Search By Startup Name'  className='my-1 rounded px-5 hidden lg:block md:block' value={Search} onChange={(e)=>setSearch(e.target.value)}/>
 
         <text class='px-10 py-1 bg-blue-400  text-white my-1 rounded cursor-pointer hidden lg:block md:block ' onClick={handleAdd}>Add Startup data</text>
       
     </div>
-
+    <input placeholder=' Search By Startup Name'  className='my-1 rounded px-5 w-4/5 mx-auto flex flex-row py-1.5 border-red-600 border-2 pl-2  md:hidden lg:hidden'  value={Search} onChange={(e)=>setSearch(e.target.value)}/>
     <text class=' w-4/5 z-0 flex flex-row bg-red-600 mx-auto my-1 text-white justify-center px-5 py-2 rounded cursor-pointer  md:hidden lg:hidden ' onClick={handleAdd}>Add Startup data</text>
     <div class="w-screen relative px-1 mx-auto my-2.5  grid grid-cols-1 gap-1   md:grid-cols-2 lg:grid-cols-3 justify-center lg:w-4/5 md:w-4/5 sm:w-4/5 ">
     {
       data.length ? (
       <>
       {
-        data && data.map((data, index)=>{
+        data && data
+        .filter((data)=>(data.Company_Name.toLowerCase().startsWith(Search.toLowerCase())))
+        .filter((data)=>(industry === "All" ? data : data.Industry === industry ))
+        .map((data, index)=>{
           return(
             <div key={index} className={"w-5/5 relative h-44 bg-blue-400 mx-auto my-4 px-5 flex flex-col align-middle lg:w-4/5 md:w-4/5 sm:w-4/5 " }>
               <text class="text-white pt-5 ">{`Company Name : ${data.Company_Name}`}</text>
